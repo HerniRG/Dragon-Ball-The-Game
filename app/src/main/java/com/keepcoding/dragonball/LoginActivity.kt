33fd36c3a8a44ebdaddc940f71f12f060fabe9d7
hernirg <hernanrg85@gmail.com>
2025-02-19 23:02:22 +0100

    binding y boton con toast

--- app/src/main/java/com/keepcoding/dragonball/LoginActivity.kt
+++ app/src/main/java/com/keepcoding/dragonball/LoginActivity.kt
@@ -1,20 +1,33 @@
 package com.keepcoding.dragonball
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.keepcoding.dragonball.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private val viewModel: LoginViewModel by viewModels()
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
 
+        viewModel.saveUser(
+            preferences = getSharedPreferences("LoginPreferences", MODE_PRIVATE),
+            user ="hernan",
+            password = "123456")
+
+        // usar binding para acceder a los elementos de la vista
+        binding.buttonLogin.setOnClickListener {
+            // si ya he pulsado y aun sigue un toast ejecutandose lo cancela
+            Toast.makeText(this, "Botón pulsado", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
--- app/src/main/java/com/keepcoding/dragonball/LoginViewModel.kt
+++ app/src/main/java/com/keepcoding/dragonball/LoginViewModel.kt
@@ -8,8 +8,22 @@
 
 package com.keepcoding.dragonball
 
+import android.content.SharedPreferences
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class LoginViewModel: ViewModel() {
 
+
+    fun saveUser(preferences: SharedPreferences?, user: String, password: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            preferences?.edit()?.apply() {
+                putString("user", user)
+                putString("password", password)
+                apply()
+            }
+        }
+    }
 }
